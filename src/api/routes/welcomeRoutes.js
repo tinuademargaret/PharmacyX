78d64a51ec13a1953705167034f0230190d5f495
tinumide <summittinuade@gmail.com>
2020-02-24 10:46:56 +0100

    initial setup

--- src/api/routes/welcomeRoutes.js
+++ src/api/routes/welcomeRoutes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 module.exports = (parentRouter) => {
     router.get('/greet', (req,res,next) => {
-        console.log('I got here')
         res.send('hey! welcome here');
     });
     parentRouter.use('/welcome', router);
